c185bf80829fbc4347b340cf589b9faea74a4897
Vendula Poncova <vponcova@redhat.com>
2018-05-27 15:17:10 +0200

    Create the main reaktor activity UI.

--- app/src/main/java/cz/jmpionyr/pstp/fusionreactor/ReactorActivity.java
+++ app/src/main/java/cz/jmpionyr/pstp/fusionreactor/ReactorActivity.java
@@ -3,11 +3,34 @@ package cz.jmpionyr.pstp.fusionreactor;
 import android.app.Activity;
 import android.os.Bundle;
 
+import cz.jmpionyr.pstp.fusionreactor.reactor.MainFragment;
+
 public class ReactorActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reactor);
+
+        // Check that the activity is using the layout version with
+        // the fragment_container FrameLayout
+        if (findViewById(R.id.fragment_container) != null) {
+
+            // However, if we're being restored from a previous state,
+            // then we don't need to do anything and should return or else
+            // we could end up with overlapping fragments.
+            if (savedInstanceState != null) {
+                return;
+            }
+
+            // Create a new Fragment to be placed in the activity layout
+            MainFragment fragment = new MainFragment();
+
+            // Add the fragment to the 'fragment_container' FrameLayout
+            getFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.fragment_container, fragment)
+                    .commit();
+        }
     }
 }
