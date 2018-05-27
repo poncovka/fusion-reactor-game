09eefe5cd6adaa506576327b32f5d9562ee6f3f6
Vendula Poncova <vponcova@redhat.com>
2018-05-27 09:47:56 +0200

    Add the experiment activity

---
+++ app/src/main/java/cz/jmpionyr/pstp/fusionreactor/ExperimentActivity.java
@@ -0,0 +1,13 @@
+package cz.jmpionyr.pstp.fusionreactor;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ExperimentActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_experiment);
+    }
+}
