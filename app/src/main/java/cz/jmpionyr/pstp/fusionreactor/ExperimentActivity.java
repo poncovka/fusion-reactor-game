1fc3d2c4d722a24237618aae5ebc69e33d0af20d
Vendula Poncova <vponcova@redhat.com>
2018-06-13 22:48:07 +0200

    Handle application state

--- app/src/main/java/cz/jmpionyr/pstp/fusionreactor/ExperimentActivity.java
+++ app/src/main/java/cz/jmpionyr/pstp/fusionreactor/ExperimentActivity.java
@@ -8,11 +8,15 @@ import android.provider.MediaStore;
 import android.util.Log;
 import android.util.SparseArray;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.vision.Frame;
 import com.google.android.gms.vision.barcode.Barcode;
 import com.google.android.gms.vision.barcode.BarcodeDetector;
+
+import java.util.Locale;
+import java.util.Random;
 
 import cz.jmpionyr.pstp.fusionreactor.experiment.LoadFragment;
 import cz.jmpionyr.pstp.fusionreactor.experiment.ReadyFragment;
@@ -20,21 +24,36 @@ import cz.jmpionyr.pstp.fusionreactor.experiment.RunFragment;
 
 public class ExperimentActivity extends Activity {
 
-    private int number;
-    private String reactant_1;
-    private String reactant_2;
+    public static final String EXPERIMENT_ID = "experimentId";
+    public static final String FIRST_REACTANT = "firstReactant";
+    public static final String SECOND_REACTANT = "secondReactant";
 
+    private int experiment_id;
+    private String first_reactant;
+    private String second_reactant;
     private BarcodeDetector detector;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        if (savedInstanceState == null) {
+            Random random = new Random();
+            experiment_id = random.nextInt(90000) + 10000;
+        }
+        else {
+            experiment_id = savedInstanceState.getInt(EXPERIMENT_ID);
+            first_reactant = savedInstanceState.getString(FIRST_REACTANT);
+            second_reactant = savedInstanceState.getString(SECOND_REACTANT);
+        }
+
         detector = new BarcodeDetector.Builder(getApplicationContext())
                 .setBarcodeFormats(Barcode.QR_CODE)
                 .build();
 
         setContentView(R.layout.activity_experiment);
+        TextView textView = findViewById(R.id.experiment_id);
+        textView.setText(String.format(Locale.getDefault(), "Experiment: #%d", experiment_id));
 
         // Check that the activity is using the layout version with
         // the fragment_container FrameLayout
@@ -69,10 +88,12 @@ public class ExperimentActivity extends Activity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == 1 && resultCode == RESULT_OK) {
             Bundle extras = data.getExtras();
-            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            if (extras == null) {
+                return;
+            }
 
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
             if (imageBitmap == null) {
-                Toast.makeText(this, "Nepodařilo se zpracovat data.", Toast.LENGTH_SHORT).show();
                 return;
             }
 
@@ -80,27 +101,33 @@ public class ExperimentActivity extends Activity {
             SparseArray<Barcode> barcodes = detector.detect(frame);
 
             if (barcodes.size() == 0) {
-                Toast.makeText(this, "Nedetekován žádný kód.", Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, "Reaktant nebyl detekován.", Toast.LENGTH_SHORT).show();
                 return;
             }
 
             Barcode barcode = barcodes.valueAt(0);
 
-            if (reactant_1 == null) {
-                reactant_1 = barcode.displayValue;
-                Toast.makeText(this, String.format("Reaktant #1: %s",reactant_1), Toast.LENGTH_SHORT).show();
+            if (first_reactant == null) {
+                first_reactant = barcode.displayValue;
+                Toast.makeText(this, String.format("Detekován reaktant #1: %s", first_reactant), Toast.LENGTH_SHORT).show();
             }
-            else if (reactant_2 == null) {
-                reactant_2 = barcode.displayValue;
-                Toast.makeText(this, String.format("Reaktant #2: %s", reactant_2), Toast.LENGTH_SHORT).show();
+            else if (second_reactant == null) {
+                second_reactant = barcode.displayValue;
+                Toast.makeText(this, String.format("Detekován reaktant #2: %s", second_reactant), Toast.LENGTH_SHORT).show();
             }
             else {
                 Toast.makeText(this, "Nelze nastavit reaktant.", Toast.LENGTH_SHORT).show();
             }
         }
 
-        if (reactant_1 != null && reactant_2 != null) {
+        if (first_reactant != null && second_reactant != null) {
             ReadyFragment fragment = new ReadyFragment();
+
+            Bundle args = new Bundle();
+            args.putString(FIRST_REACTANT, first_reactant);
+            args.putString(SECOND_REACTANT, second_reactant);
+            fragment.setArguments(args);
+
             getFragmentManager()
                     .beginTransaction()
                     .replace(R.id.fragment_container, fragment)
@@ -110,9 +137,24 @@ public class ExperimentActivity extends Activity {
 
     public void runExperiment(View view) {
         RunFragment fragment = new RunFragment();
+
+        Bundle args = new Bundle();
+        args.putString(FIRST_REACTANT, first_reactant);
+        args.putString(SECOND_REACTANT, second_reactant);
+        fragment.setArguments(args);
+
         getFragmentManager()
                 .beginTransaction()
                 .replace(R.id.fragment_container, fragment)
                 .commit();
     }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putInt(EXPERIMENT_ID, experiment_id);
+        outState.putString(FIRST_REACTANT, first_reactant);
+        outState.putString(SECOND_REACTANT, second_reactant);
+        super.onSaveInstanceState(outState);
+    }
+
 }
--- app/src/main/java/cz/jmpionyr/pstp/fusionreactor/experiment/ReadyFragment.java
+++ app/src/main/java/cz/jmpionyr/pstp/fusionreactor/experiment/ReadyFragment.java
@@ -3,10 +3,15 @@ package cz.jmpionyr.pstp.fusionreactor.experiment;
 
 import android.os.Bundle;
 import android.app.Fragment;
+import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import java.util.Locale;
+
+import cz.jmpionyr.pstp.fusionreactor.ExperimentActivity;
 import cz.jmpionyr.pstp.fusionreactor.R;
 
 /**
@@ -19,7 +24,6 @@ public class ReadyFragment extends Fragment {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -27,4 +31,21 @@ public class ReadyFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_ready, container, false);
     }
 
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        String reactant;
+        TextView text_view;
+        Bundle args = getArguments();
+
+        // Set the first reactant.
+        reactant = args.getString(ExperimentActivity.FIRST_REACTANT);
+        text_view = getActivity().findViewById(R.id.first_reactant_text_view);
+        text_view.setText(String.format("Reaktant #1: %s", reactant));
+
+        // Set the second reactant.
+        reactant = args.getString(ExperimentActivity.SECOND_REACTANT);
+        text_view = getActivity().findViewById(R.id.second_reactant_text_view);
+        text_view.setText(String.format("Reaktant #2: %s", reactant));
+    }
 }
