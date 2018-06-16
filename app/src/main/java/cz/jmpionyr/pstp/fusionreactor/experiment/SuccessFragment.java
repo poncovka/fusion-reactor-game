326aa82408a7c48d3c50afaec262a2c2fe21a262
Vendula Poncova <vponcova@redhat.com>
2018-06-16 14:45:57 +0200

    Fix the product name

--- app/src/main/java/cz/jmpionyr/pstp/fusionreactor/experiment/SuccessFragment.java
+++ app/src/main/java/cz/jmpionyr/pstp/fusionreactor/experiment/SuccessFragment.java
@@ -3,10 +3,13 @@ package cz.jmpionyr.pstp.fusionreactor.experiment;
 
 import android.os.Bundle;
 import android.app.Fragment;
+import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import cz.jmpionyr.pstp.fusionreactor.ExperimentActivity;
 import cz.jmpionyr.pstp.fusionreactor.R;
 
 /**
@@ -27,4 +30,15 @@ public class SuccessFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_success, container, false);
     }
 
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        Bundle args = getArguments();
+
+        String product = args.getString(ExperimentActivity.PRODUCT);
+        TextView text_view = getActivity().findViewById(R.id.product_text_view);
+        text_view.setText(String.format("Vytvořen nový produkt: %s", product));
+
+    }
 }
