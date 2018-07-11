58bddce55210a57a3ffc88fc4cb86455afae09f5
Vendula Poncova <vponcova@redhat.com>
2018-07-11 14:17:03 +0200

    Move camera logic to the CameraPreview

--- app/src/main/java/cz/jmpionyr/pstp/fusionreactor/loader/CameraPreview.java
+++ app/src/main/java/cz/jmpionyr/pstp/fusionreactor/loader/CameraPreview.java
@@ -1,29 +1,43 @@
 package cz.jmpionyr.pstp.fusionreactor.loader;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.content.res.Configuration;
-import android.content.res.TypedArray;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
 import android.graphics.SurfaceTexture;
-import android.graphics.drawable.Drawable;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCaptureSession;
 import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraDevice;
+import android.hardware.camera2.CameraManager;
+import android.hardware.camera2.CaptureRequest;
 import android.hardware.camera2.params.StreamConfigurationMap;
-import android.text.TextPaint;
+import android.os.Handler;
+import android.os.Message;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.util.Size;
+import android.view.Surface;
 import android.view.TextureView;
-import android.view.View;
 
-import cz.jmpionyr.pstp.fusionreactor.R;
+import java.util.Collections;
 
 /**
  * TODO: document your custom view class.
  */
 public class CameraPreview extends TextureView {
 
+    private static final String TAG = "CameraPreview";
+    public static final int BITMAP_AVAILABLE = 1;
+
     private Size ratio;
+    private Handler handler;
+    private Surface surface;
+
+    private CameraDevice cameraDevice;
+    private CameraCaptureSession cameraSession;
 
     public CameraPreview(Context context) {
         super(context);
@@ -40,6 +54,14 @@ public class CameraPreview extends TextureView {
         init(attrs, defStyle);
     }
 
+    private void init(AttributeSet attrs, int defStyle) {
+        this.setSurfaceTextureListener(surfaceListener);
+    }
+
+    public void setBitmapHandler(Handler handler) {
+        this.handler = handler;
+    }
+
     public void setRatio(Size ratio) {
 
         if (ratio == null || ratio.getWidth() == 0 || ratio.getHeight() == 0) {
@@ -49,9 +71,65 @@ public class CameraPreview extends TextureView {
         this.ratio = ratio;
     }
 
-    public void applyCameraCharacteristics(CameraCharacteristics characteristics, int orientation) {
-        StreamConfigurationMap configurationMap = characteristics.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP);
+    public void reload() {
+        CameraManager cameraManager = getCameraManager();
+        String cameraID = getCamera(cameraManager);
 
+        CameraCharacteristics characteristics = getCameraCharacteristics(cameraManager, cameraID);
+        int orientation = getResources().getConfiguration().orientation;
+        applyCameraCharacteristics(characteristics, orientation);
+
+        openCamera(cameraManager, cameraID);
+    }
+
+    private CameraManager getCameraManager() {
+        return (CameraManager) getContext().getSystemService(Context.CAMERA_SERVICE);
+    }
+
+    private String getCamera(CameraManager cameraManager) {
+
+        if (cameraManager == null) {
+            return null;
+        }
+
+        try {
+            for (String cameraID : cameraManager.getCameraIdList()) {
+                CameraCharacteristics characteristics = cameraManager.getCameraCharacteristics(cameraID);
+                Integer facing = characteristics.get(CameraCharacteristics.LENS_FACING);
+                if (facing != null && facing == CameraCharacteristics.LENS_FACING_FRONT) {
+                    continue;
+                }
+
+                Log.d(TAG, String.format("Found camera with ID: %s", cameraID));
+                return cameraID;
+            }
+        } catch (CameraAccessException e) {
+            Log.e(TAG, "Camera failed to be found.", e);
+        }
+
+        Log.d(TAG, "No camera was found.");
+        return null;
+    }
+
+    private CameraCharacteristics getCameraCharacteristics(CameraManager cameraManager, String cameraID) {
+
+        if (cameraManager == null || cameraID == null) {
+            return null;
+        }
+
+        try {
+            return cameraManager.getCameraCharacteristics(cameraID);
+
+        } catch (CameraAccessException e) {
+            Log.e(TAG, "Camera characteristics failed.", e);
+        }
+
+        return null;
+    }
+
+    public void applyCameraCharacteristics(CameraCharacteristics characteristics, int orientation) {
+
+        StreamConfigurationMap configurationMap = characteristics.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP);
         if (configurationMap == null) {
             return;
         }
@@ -69,6 +147,44 @@ public class CameraPreview extends TextureView {
 
         setRatio(ratio);
         requestLayout();
+
+        surface = new Surface(getSurfaceTexture());
+    }
+
+
+    private void openCamera(CameraManager cameraManager, String cameraID) {
+
+        // Check the camera permissions.
+        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+
+        // Open the camera.
+        try {
+            cameraManager.openCamera(cameraID, cameraStateCallback, new Handler());
+        } catch (CameraAccessException e) {
+            Log.e(TAG, "Camera couldn't be opened.", e);
+        }
+    }
+
+    private void createPreviewSession(Surface surface, CameraDevice cameraDevice) {
+        try {
+            cameraDevice.createCaptureSession(Collections.singletonList(surface), sessionStateCallback, new Handler());
+        } catch (CameraAccessException e) {
+            Log.e(TAG, "Session couldn't be created", e);
+        }
+    }
+
+    private CaptureRequest createPreviewRequest(Surface surface, CameraDevice cameraDevice) {
+        try {
+            CaptureRequest.Builder requestBuilder = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
+            requestBuilder.addTarget(surface);
+            return requestBuilder.build();
+        } catch (CameraAccessException e) {
+            Log.e(TAG, "Request couldn't be created.", e);
+        }
+
+        return null;
     }
 
     @Override
@@ -89,8 +205,116 @@ public class CameraPreview extends TextureView {
         }
     }
 
-    private void init(AttributeSet attrs, int defStyle) {
+    private final TextureView.SurfaceTextureListener surfaceListener = new TextureView.SurfaceTextureListener() {
 
+        @Override
+        public void onSurfaceTextureAvailable(SurfaceTexture surfaceTexture, int width, int height) {
+            // Surface is ready.
+            Log.d(TAG, "The surface is ready.");
+            surface = new Surface(surfaceTexture);
+
+            // Wait for camera.
+            reload();
+        }
+
+        @Override
+        public void onSurfaceTextureSizeChanged(SurfaceTexture surface, int width, int height) {
+
+        }
+
+        @Override
+        public boolean onSurfaceTextureDestroyed(SurfaceTexture surface) {
+            return false;
+        }
+
+        @Override
+        public void onSurfaceTextureUpdated(SurfaceTexture surface) {
+            if (handler == null) {
+                return;
+            }
+
+            Message msg = Message.obtain();
+            msg.what = BITMAP_AVAILABLE;
+            msg.obj = getBitmap();
+            handler.sendMessage(msg);
+        }
+    };
+
+    private final CameraDevice.StateCallback cameraStateCallback = new CameraDevice.StateCallback() {
+
+        @Override
+        public void onOpened(@NonNull CameraDevice camera) {
+            // Camera is ready.
+            Log.d(TAG, "The camera is ready.");
+            cameraDevice = camera;
+
+            // Wait for a preview session.
+            createPreviewSession(surface, cameraDevice);
+        }
+
+        @Override
+        public void onDisconnected(@NonNull CameraDevice camera) {
+            Log.d(TAG, "Camera is disconnected.");
+            closeCamera();
+        }
+
+        @Override
+        public void onError(@NonNull CameraDevice camera, int error) {
+            Log.d(TAG, String.format("Camera failed with %d.", error));
+            closeCamera();
+        }
+    };
+
+    private final CameraCaptureSession.StateCallback sessionStateCallback = new CameraCaptureSession.StateCallback() {
+        @Override
+        public void onConfigured(@NonNull CameraCaptureSession session) {
+            Log.d(TAG, "The session is ready.");
+
+            // Session is ready.
+            cameraSession = session;
+
+            // Create the preview request.
+            CaptureRequest request = createPreviewRequest(surface, cameraDevice);
+            if (request == null) {
+                Log.e(TAG, "The request failed to be created.");
+                return;
+            }
+
+            // Wait for the preview request.
+            try {
+                session.setRepeatingRequest(request, null, new Handler());
+            } catch (CameraAccessException e) {
+                Log.e(TAG, "The request failed to be set.", e);
+            }
+        }
+
+        @Override
+        public void onConfigureFailed(@NonNull CameraCaptureSession session) {
+            Log.e(TAG, "Session configuration has failed.");
+            closeSession();
+        }
+    };
+
+    private void closeCamera() {
+        if (cameraDevice != null) {
+            cameraDevice.close();
+            cameraDevice = null;
+        }
     }
 
+    private void closeSession() {
+        if (cameraSession != null) {
+            cameraSession.close();
+            cameraSession = null;
+        }
+    }
+
+    public void close() {
+        closeCamera();
+        closeSession();
+
+        ratio = null;
+        surface = null;
+        handler = null;
+    }
 }
--- app/src/main/java/cz/jmpionyr/pstp/fusionreactor/loader/LoaderActivity.java
+++ app/src/main/java/cz/jmpionyr/pstp/fusionreactor/loader/LoaderActivity.java
@@ -48,8 +48,7 @@ public class LoaderActivity extends Activity {
 
     private static final String TAG = "LoaderActivity";
     private static final int CAMERA_PERMISSION_REQUEST = 1;
-    private static final int DETECT_CODE_MESSAGE = 1;
-    private static final int SET_REACTANT_MESSAGE = 2;
+    private static final int SET_REACTANT_MESSAGE = 1;
 
     private String first_reactant;
     private String second_reactant;
@@ -57,10 +56,6 @@ public class LoaderActivity extends Activity {
     private Handler main_handler;
 
     private CameraPreview cameraPreview;
-    private Surface surface;
-    private CameraDevice cameraDevice;
-    private CameraCaptureSession cameraSession;
-
     private BarcodeDetector detector;
     private HandlerThread detector_thread;
     private Handler detector_handler;
@@ -86,25 +81,6 @@ public class LoaderActivity extends Activity {
 
             return true;
         }
-
-        private String detectBarcode(Bitmap imageBitmap) {
-            // Log.d(TAG, "Processing the bitmap.");
-
-            if (imageBitmap == null) {
-                Log.d(TAG, "No bitmap to process.");
-                return null;
-            }
-
-            Frame frame = new Frame.Builder().setBitmap(imageBitmap).build();
-            SparseArray<Barcode> barcodes = detector.detect(frame);
-
-            if (barcodes.size() == 0) {
-                return null;
-            }
-
-            Barcode barcode = barcodes.valueAt(0);
-            return barcode.displayValue;
-        }
     };
 
     private final Handler.Callback main_callback = new Handler.Callback() {
@@ -120,6 +96,88 @@ public class LoaderActivity extends Activity {
             return true;
         }
     };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loader);
+
+        // Start the main handler.
+        main_handler = new Handler(main_callback);
+
+        // Start the background thread.
+        detector_thread = new HandlerThread("BarcodeDetection");
+        detector_thread.start();
+
+        // Start the background handler.
+        detector_handler = new Handler(detector_thread.getLooper(), detector_callback);
+
+        // Wait for surface.
+        cameraPreview = findViewById(R.id.camera_preview);
+        cameraPreview.setBitmapHandler(detector_handler);
+
+        // Create the detector.
+        detector = new BarcodeDetector.Builder(getApplicationContext())
+                .setBarcodeFormats(Barcode.QR_CODE)
+                .build();
+
+        checkCameraPermissions();
+    }
+
+    private void startCameraPreview() {
+        if (checkCameraPermissions()) {
+            cameraPreview.reload();
+        }
+    }
+
+    private boolean checkCameraPermissions() {
+        // Check the camera permissions.
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.CAMERA}, CAMERA_PERMISSION_REQUEST);
+            Log.d(TAG, "Camera permissions requested.");
+            return false;
+        }
+
+        return true;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == CAMERA_PERMISSION_REQUEST) {
+            // If request is cancelled, the result arrays are empty.
+            if (grantResults.length > 0
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // permission was granted, yay! Do the
+                // contacts-related task you need to do.
+                startCameraPreview();
+            } else {
+                // permission denied, boo! Disable the
+                // functionality that depends on this permission.
+                Log.e(TAG, "Permissions are not granted.");
+            }
+        }
+    }
+
+    private String detectBarcode(Bitmap imageBitmap) {
+        // Log.d(TAG, "Processing the bitmap.");
+
+        if (imageBitmap == null) {
+            Log.d(TAG, "No bitmap to process.");
+            return null;
+        }
+
+        Frame frame = new Frame.Builder().setBitmap(imageBitmap).build();
+        SparseArray<Barcode> barcodes = detector.detect(frame);
+
+        if (barcodes.size() == 0) {
+            return null;
+        }
+
+        Barcode barcode = barcodes.valueAt(0);
+        return barcode.displayValue;
+    }
 
     protected void setReactant(String reactant) {
         String message = "Nelze nastavit reaktant.";
@@ -149,237 +207,15 @@ public class LoaderActivity extends Activity {
     }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_loader);
-
-        // Start the main handler.
-        main_handler = new Handler(main_callback);
-
-        // Start the background thread.
-        detector_thread = new HandlerThread("BarcodeDetection");
-        detector_thread.start();
-
-        // Start the background handler.
-        detector_handler = new Handler(detector_thread.getLooper(), detector_callback);
-
-        // Wait for surface.
-        cameraPreview = findViewById(R.id.camera_preview);
-        cameraPreview.setSurfaceTextureListener(surfaceListener);
-
-        // Create the detector.
-        detector = new BarcodeDetector.Builder(getApplicationContext())
-                .setBarcodeFormats(Barcode.QR_CODE)
-                .build();
-
-    }
-
-    private final TextureView.SurfaceTextureListener surfaceListener = new TextureView.SurfaceTextureListener() {
-        @Override
-        public void onSurfaceTextureAvailable(SurfaceTexture surfaceTexture, int width, int height) {
-            // Surface is ready.
-            Log.d(TAG, "The surface is ready.");
-            surface = new Surface(surfaceTexture);
-
-            // Wait for camera.
-            openCamera();
-        }
-
-        @Override
-        public void onSurfaceTextureSizeChanged(SurfaceTexture surface, int width, int height) {
-
-        }
-
-        @Override
-        public boolean onSurfaceTextureDestroyed(SurfaceTexture surface) {
-            return false;
-        }
-
-        @Override
-        public void onSurfaceTextureUpdated(SurfaceTexture surface) {
-            if (detector_handler == null) {
-                return;
-            }
-
-            Message msg = Message.obtain();
-            msg.what = DETECT_CODE_MESSAGE;
-            msg.obj = cameraPreview.getBitmap();
-            detector_handler.sendMessage(msg);
-        }
-    };
-
-    private final CameraDevice.StateCallback cameraStateCallback = new CameraDevice.StateCallback() {
-
-        @Override
-        public void onOpened(@NonNull CameraDevice camera) {
-            // Camera is ready.
-            Log.d(TAG, "The camera is ready.");
-            cameraDevice = camera;
-
-            // Wait for a preview session.
-            createPreviewSession(surface, cameraDevice);
-        }
-
-        @Override
-        public void onDisconnected(@NonNull CameraDevice camera) {
-            Log.d(TAG, "Camera is disconnected.");
-            cameraDevice = null;
-        }
-
-        @Override
-        public void onError(@NonNull CameraDevice camera, int error) {
-            Log.d(TAG, String.format("Camera failed with %d.", error));
-            cameraDevice = null;
-        }
-    };
-
-    private final CameraCaptureSession.StateCallback sessionStateCallback = new CameraCaptureSession.StateCallback() {
-        @Override
-        public void onConfigured(@NonNull CameraCaptureSession session) {
-            Log.d(TAG, "The session is ready.");
-
-            // Session is ready.
-            cameraSession = session;
-
-            // Create the preview request.
-            CaptureRequest request = createPreviewRequest(surface, cameraDevice);
-            if (request == null) {
-                Log.e(TAG, "The request failed to be created.");
-                return;
-            }
-
-            // Wait for the preview request.
-            try {
-                session.setRepeatingRequest(request, null, new Handler());
-            } catch (CameraAccessException e) {
-                Log.e(TAG, "The request failed to be set.", e);
-            }
-        }
-
-        @Override
-        public void onConfigureFailed(@NonNull CameraCaptureSession session) {
-            Log.e(TAG, "Session configuration has failed.");
-        }
-    };
-
-    private String getCamera(CameraManager cameraManager) {
-
-        try {
-            for (String cameraID : cameraManager.getCameraIdList()) {
-                CameraCharacteristics characteristics = cameraManager.getCameraCharacteristics(cameraID);
-                Integer facing = characteristics.get(CameraCharacteristics.LENS_FACING);
-                if (facing != null && facing == CameraCharacteristics.LENS_FACING_FRONT) {
-                    continue;
-                }
-
-                Log.d(TAG, String.format("Found camera: %s", cameraID));
-                return cameraID;
-            }
-        } catch (CameraAccessException e) {
-            Log.e(TAG, "Camera failed to be found.", e);
-        }
-
-        Log.d(TAG, "No camera was found.");
-        return null;
-    }
-
-    private void openCamera() {
-
-        // Check the camera permissions.
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.CAMERA}, CAMERA_PERMISSION_REQUEST);
-            Log.d(TAG, "Camera permissions requested.");
-            return;
-        }
-
-        // Get the camera manager.
-        CameraManager cameraManager = (CameraManager) getSystemService(CAMERA_SERVICE);
-        if (cameraManager == null) {
-            Log.e(TAG, "Camera manager doesn't exist.");
-            return;
-        }
-
-        // Get the camera ID.
-        String cameraID = getCamera(cameraManager);
-        if (cameraID == null) {
-            Log.e(TAG, "No camera found.");
-            return;
-        }
-
-        try {
-            CameraCharacteristics characteristics = cameraManager.getCameraCharacteristics(cameraID);
-            int orientation = getResources().getConfiguration().orientation;
-            cameraPreview.applyCameraCharacteristics(characteristics, orientation);
-            surface = new Surface(cameraPreview.getSurfaceTexture());
-
-        } catch (CameraAccessException e) {
-            Log.e(TAG, "Camera characteristics failed.", e);
-        }
-
-
-        // Open the camera.
-        try {
-            cameraManager.openCamera(cameraID, cameraStateCallback, new Handler());
-        } catch (CameraAccessException e) {
-            Log.e(TAG, "Camera couldn't be opened.", e);
-        }
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-
-        if (requestCode == CAMERA_PERMISSION_REQUEST) {
-            // If request is cancelled, the result arrays are empty.
-            if (grantResults.length > 0
-                    && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                // permission was granted, yay! Do the
-                // contacts-related task you need to do.
-                openCamera();
-            } else {
-                // permission denied, boo! Disable the
-                // functionality that depends on this permission.
-                Log.e(TAG, "Permissions are not granted.");
-            }
-        }
-    }
-
-    private void createPreviewSession(Surface surface, CameraDevice cameraDevice) {
-        try {
-            cameraDevice.createCaptureSession(Collections.singletonList(surface), sessionStateCallback, new Handler());
-        } catch (CameraAccessException e) {
-            Log.e(TAG, "Session couldn't be created", e);
-        }
-    }
-
-    private CaptureRequest createPreviewRequest(Surface surface, CameraDevice cameraDevice) {
-        try {
-            CaptureRequest.Builder requestBuilder = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
-            requestBuilder.addTarget(surface);
-            return requestBuilder.build();
-        } catch (CameraAccessException e) {
-            Log.e(TAG, "Request couldn't be created.", e);
-        }
-
-        return null;
-    }
-
-    @Override
     protected void onPause() {
-
-        if (cameraSession != null) {
-            try {
-                cameraSession.abortCaptures();
-            } catch (CameraAccessException e) {
-                Log.e(TAG, "Failed to abort captures.", e);
-            }
-            cameraSession.close();
-            cameraSession = null;
+        if (cameraPreview != null) {
+            cameraPreview.close();
+            cameraPreview = null;
         }
 
-        if (cameraDevice != null) {
-            cameraDevice.close();
-            cameraDevice = null;
+        if (detector_handler != null) {
+            detector_handler.removeCallbacksAndMessages(null);
+            detector_handler = null;
         }
 
         if (detector_thread != null) {
@@ -390,7 +226,6 @@ public class LoaderActivity extends Activity {
                 Log.e(TAG, "Failed to join the detector thread.", e);
             }
             detector_thread = null;
-            detector_handler = null;
         }
 
         if (detector != null) {
